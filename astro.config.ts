236b99b5e811dee0976d130315f18e9cb0f784de
Hidayat Taufiqur <hidayattaufiqur@gmail.com>
2024-06-09 11:29:44 +0700

    feat(config): add node entry to run the site as SSR

--- astro.config.ts
+++ astro.config.ts
@@ -3,8 +3,14 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import UnoCSS from 'unocss/astro'
 import vue from '@astrojs/vue'
+import node from '@astrojs/node'
 
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+
   site: 'https://astro-theme-vitesse.netlify.app/',
   server: {
     port: 1977,
